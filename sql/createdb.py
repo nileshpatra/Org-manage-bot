112cf150653b5582556188ea469d976485c978ef
nilesh <npatra974@gmail.com>
2019-07-19 18:02:03 +0530

    add required sql db

--- sql/createdb.py
+++ sql/createdb.py
@@ -1,17 +1,18 @@
 import sqlite3
-connection = sqlite3.connect("filter.db")
+connection = sqlite3.connect("meetings.db")
 
 cursor = connection.cursor()
 
 sql_command = """
-CREATE TABLE message ( 
-keyword VARCHAR(30) PRIMARY KEY, 
-response VARCHAR(30) 
+CREATE TABLE meetup ( 
+Date VARCHAR(30) PRIMARY KEY, 
+Time VARCHAR(30),
+Topic VARCHAR(30) 
 );"""
 cursor.execute(sql_command)
 
-sql_command = """INSERT INTO message (keyword, response)
-    VALUES ("1" , "first commit");"""
+sql_command = """INSERT INTO meetup (Date, Time, Topic)
+    VALUES ("29_may" , "5" , "kernel");"""
 cursor.execute(sql_command)
 
 connection.commit()
