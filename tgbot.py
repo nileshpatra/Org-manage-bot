aca2099a795fe3a697debb1e79a5cec3c6c8aa59
Nilesh <npatra974@gmail.com>
2020-01-06 19:47:33 +0530

    Fix lint, cleaned code

--- tgbot.py
+++ tgbot.py
@@ -2,233 +2,258 @@ import base64
 from io import BytesIO
 from PIL import Image
 from telegram import Message, Update, Bot, User
-from modules.thonkify_dict import thonkifydict , soc
-from telegram.ext import CommandHandler , Updater , MessageHandler , Filters , run_async
+from modules.thonkify_dict import thonkifydict, soc
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, run_async
 import requests
 import sqlite3
 import sql
 import configparser
 
-#Bot token goes here
-TOKEN = ''
+
 def CheckConfig():
-        '''
-        ChekConfig() funtion return the token from config.ini file
-        it will return False if token is empty or config.ini file structure changed
-        '''
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        TOKEN= ""
-        try:
-                token = config['telegram']['token']
-                
-                if token == "" or token == "TOKEN_KEY_WITHOUT_QUOTES":
-                        print("Token Should not be Empty please add it in config.ini file")
-                        return False
-                elif '"' in token or "'" in token:
-                        print("Do not add Quotes to token use direct value\ntoken=XXXX:XXXXXX")
-                        return False
-                        
-                else:
-                        return token
-        except:
-                print('Do not change Syntax of config.ini file!')
-                return False
+
+    '''
+    ChekConfig() funtion return the token from config.ini file
+    it will return False if token is empty or config.ini file structure changed
+    '''
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    try:
+        token = config['telegram']['token']
+
+        if token == "" or token == "TOKEN_KEY_WITHOUT_QUOTES":
+            print("Token Should not be Empty please add it in config.ini file")
+            return False
+        elif '"' in token or "'" in token:
+            print("Do not add Quotes to token use direct value\ntoken=XXXX:XXXXXX")
+            return False
+
+        else:
+            return token
+    except:
+        print('Do not change Syntax of config.ini file!')
+        return False
 
 
-#Initial Introduction of the bot
-def startmessage(bot , update):
-	chat_id = update.message.chat_id
-	bot.send_message(chat_id = chat_id , text = '''Greetings! My name is org_manage-bot , I'm here to help 
-			you manage you telegram group''' )
+# Initial Introduction of the bot
+def startmessage(bot, update):
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id=chat_id, text='''Greetings! My name is org_manage-bot , I'm here to help
+            you manage you telegram group''')
 
-#Lists all the commands the bot has
-def helpbot(bot , update):
-	chat_id = update.message.chat_id
-	text = '''
-	Hey! These are the following commands availaible:
-		/start - get started using org-bot
-		/sendlogo - sends the logo of the company
-		/addmeeting - add new meting time date and topic
-		/next - gives details about the next meeting
-		/social - add new social media handle
-		/github - gets the github link for the organization
-		/facebook - gets the facebook link for the organization
-		/website - gets the website link for the organization
-		/meetup - gets the meetup link for the organization
-		/remove - removes the user when you type this in refering to their message
-		/filter - replies to the message when a particular keyword is entered
-		/reply - replies to the message if it has been added a 'filter' to
-		/thonkify - sends a funny image png when you reply to the message
-	'''
-	bot.send_message(chat_id = chat_id , text = text)	
+# Lists all the commands the bot has
+
+
+def helpbot(bot, update):
+    chat_id = update.message.chat_id
+    text = '''
+    Hey! These are the following commands availaible:
+        /start - get started using org-bot
+        /sendlogo - sends the logo of the company
+        /addmeeting - add new meting time date and topic
+        /next - gives details about the next meeting
+        /social - add new social media handle
+        /github - gets the github link for the organization
+        /facebook - gets the facebook link for the organization
+        /website - gets the website link for the organization
+        /meetup - gets the meetup link for the organization
+        /remove - removes the user when you type this in refering to their message
+        /filter - replies to the message when a particular keyword is entered
+        /reply - replies to the message if it has been added a 'filter' to
+        /thonkify - sends a funny image png when you reply to the message
+    '''
+    bot.send_message(chat_id=chat_id, text=text)
 
 # Welcome message when a new user join the group
-def welcome(bot , update):
-	chat_id = update.message.chat_id
-	text = 'Hello '+update.message.new_chat_members[0].first_name+'! welcome to '+update.message.chat.title+':)'
-	bot.send_message(chat_id = chat_id,
-		text = text)
+
+
+def welcome(bot, update):
+    chat_id = update.message.chat_id
+    text = 'Hello ' + update.message.new_chat_members[0].first_name + '! welcome to ' + update.message.chat.title + ':)'
+    bot.send_message(chat_id=chat_id, text=text)
+
 
 def welcome_message(bot, update):
-	if update.message.new_chat_members != []:
-		return welcome(bot, update)
+    if update.message.new_chat_members != []:
+        return welcome(bot, update)
 
 # Unwanted users can be removed via this command
-def remove_user(bot , update):
-	message = update.effective_message 
-	prev_message = message.reply_to_message
-	user_id = prev_message.from_user.id
-	prev_message.chat.kick_member(user_id)
+
+
+def remove_user(bot, update):
+    message = update.effective_message
+    prev_message = message.reply_to_message
+    user_id = prev_message.from_user.id
+    prev_message.chat.kick_member(user_id)
+
 
 def extract_effective(text):
-	netstr = ''
-	for word in text.split(' ')[2:]:
-		netstr += word
-	return text.split(' ')[1] , netstr
+    netstr = ''
+    for word in text.split(' ')[2:]:
+        netstr += word
+    return text.split(' ')[1], netstr
 
 # Add new socialmedia handle, organization can ad as many handles as they wish to
+
+
 def add_socialmedia(bot, update):
-	msg = update.effective_message.text
-	key , link = extract_effective(msg)
-	soc[key.lower()] = link
+    msg = update.effective_message.text
+    key, link = extract_effective(msg)
+    soc[key.lower()] = link
 
 # Get link of the requested social media handle
-def getlink(bot,update):
-	message = update.effective_message.text
-	key = message[1:]
-	chat_id = update.message.chat_id
-	if key in soc:
-		bot.send_message(chat_id = chat_id, text = soc[key])
-	else:
-		resp = 'Hander not added yet, to add it type : /social ', key,' <your link>'
-		bot.send_message(chat_id = chat_id , text = resp )
+
+
+def getlink(bot, update):
+    message = update.effective_message.text
+    key = message[1:]
+    chat_id = update.message.chat_id
+    if key in soc:
+        bot.send_message(chat_id=chat_id, text=soc[key])
+    else:
+        resp = 'Hander not added yet, to add it type : /social ', key, ' <your link>'
+        bot.send_message(chat_id=chat_id, text=resp)
 
 # Sends organization logo if present
-def sendlogo(bot , update):
-	chat_id = update.message.chat_id
-	if 'logo' in soc:
-		bot.send_photo(chat_id = chat_id , photo = soc['logo'])	
-	else:
-		resp = 'Hander not added yet, to add it type : /social logo <your link>'
-		bot.send_message(chat_id = chat_id , text = 'Hander not added yet, to add it type : /social logo <your link>')
+
+
+def sendlogo(bot, update):
+    chat_id = update.message.chat_id
+    if 'logo' in soc:
+        bot.send_photo(chat_id=chat_id, photo=soc['logo'])
+    else:
+        bot.send_message(
+            chat_id=chat_id, text='Hander not added yet, to add it type : /social logo <your link>')
 
 # Used to add a new meeting
-def add_meeting(bot,update):
-	connection = sqlite3.connect("sql/meetings.db")
-	cursor = connection.cursor()
-	chat = update.effective_chat
-	chat_id = update.message.chat_id
-	msg = update.effective_message.text
-	_ , date ,time , topic = msg.split(" ")
-	sql_command = """INSERT INTO meetup (Date, Time, Topic)
-	VALUES ("{date}" , "{time}" , "{topic}");"""
-	sql_command = sql_command.format(date = date , time = time , topic = topic)
-	cursor.execute(sql_command)
-	connection.commit()
-	connection.close()	
-	bot.send_message(chat_id = chat_id , text = 'command successfully added!')
+
+
+def add_meeting(bot, update):
+    connection = sqlite3.connect("sql/meetings.db")
+    cursor = connection.cursor()
+    chat_id = update.message.chat_id
+    msg = update.effective_message.text
+    _, date, time, topic = msg.split(" ")
+    sql_command = """INSERT INTO meetup (Date, Time, Topic)
+    VALUES ("{date}" , "{time}" , "{topic}");"""
+    sql_command = sql_command.format(date=date, time=time, topic=topic)
+    cursor.execute(sql_command)
+    connection.commit()
+    connection.close()
+    bot.send_message(chat_id=chat_id, text='command successfully added!')
 
 # Get the latest added meeting
-def get_list(bot,update):
-	connection = sqlite3.connect("sql/meetings.db")
-	cursor = connection.cursor()
-	cursor.execute("SELECT * FROM meetup;")
-	chat_id = update.message.chat_id
-	last_row = cursor.fetchall()[-1]
-	text = 'Date: '+last_row[0]+' Time: '+last_row[1]+' Topic: '+last_row[2]
-	bot.send_message(chat_id = chat_id , text = text)
-	connection.close()
+
+
+def get_list(bot, update):
+    connection = sqlite3.connect("sql/meetings.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM meetup;")
+    chat_id = update.message.chat_id
+    last_row = cursor.fetchall()[-1]
+    text = 'Date: ' + last_row[0] + ' Time: ' + last_row[1] + ' Topic: ' + last_row[2]
+    bot.send_message(chat_id=chat_id, text=text)
+    connection.close()
 
 # For fun -- A function which sends animated text on command
 # I have used some of this code and the thonkify_dict from the skittlesbot code
+
+
 @run_async
-def thonkify(bot , update):
-	message = update.effective_message 
-	msg = message.reply_to_message.text
-	if len(msg) > 39:
-		message.reply_text('thonk yourself!');
-		return
+def thonkify(bot, update):
+    message = update.effective_message
+    msg = message.reply_to_message.text
+    if len(msg) > 39:
+        message.reply_text('thonk yourself!')
+        return
 
-	tracking = Image.open(BytesIO(base64.b64decode('iVBORw0KGgoAAAANSUhEUgAAAAYAAAOACAYAAAAZzQIQAAAALElEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAPwZV4AAAfA8WFIAAAAASUVORK5CYII='))) # base64 encoded empty image(but longer)
-	x = 0
-	y = 896
-	image = Image.new('RGBA', [x, y], (0, 0, 0))
-	for character in msg:
-		value = thonkifydict.get(character)
-		addedimg = Image.new('RGBA', [x + value.size[0] + tracking.size[0], y], (0, 0, 0))
-		addedimg.paste(image, [0, 0])
-		addedimg.paste(tracking, [x, 0])
-		addedimg.paste(value, [x + tracking.size[0], 0])
-		image = addedimg	
-		x = x + value.size[0] + tracking.size[0]
+    # base64 encoded empty image(but longer)
+    tracking = Image.open(BytesIO(base64.b64decode(
+        'iVBORw0KGgoAAAANSUhEUgAAAAYAAAOACAYAAAAZzQIQAAAALElEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAPwZV4AAAfA8WFIAAAAASUVORK5CYII=')))
+    x = 0
+    y = 896
+    image = Image.new('RGBA', [x, y], (0, 0, 0))
+    for character in msg:
+        value = thonkifydict.get(character)
+        addedimg = Image.new(
+            'RGBA', [x + value.size[0] + tracking.size[0], y], (0, 0, 0))
+        addedimg.paste(image, [0, 0])
+        addedimg.paste(tracking, [x, 0])
+        addedimg.paste(value, [x + tracking.size[0], 0])
+        image = addedimg
+        x = x + value.size[0] + tracking.size[0]
 
-	maxsize = 1024, 896
-	if image.size[0] > maxsize[0]:
-		image.thumbnail(maxsize, Image.ANTIALIAS)
+    maxsize = 1024, 896
+    if image.size[0] > maxsize[0]:
+        image.thumbnail(maxsize, Image.ANTIALIAS)
 
-	with BytesIO() as buffer:
-		buffer.name = 'image.png'
-		image.save(buffer, 'PNG')
-		buffer.seek(0)
-		bot.send_sticker(chat_id=message.chat_id, sticker=buffer)
+    with BytesIO() as buffer:
+        buffer.name = 'image.png'
+        image.save(buffer, 'PNG')
+        buffer.seek(0)
+        bot.send_sticker(chat_id=message.chat_id, sticker=buffer)
 
 # Adds a filter - for the bot to respond to the message whenever a valid filter is encountered
-def filter_resp(bot , update):
-	connection = sqlite3.connect("sql/filter.db")
-	cursor = connection.cursor()
-	chat = update.effective_chat
-	chat_id = update.message.chat_id
-	msg = update.effective_message.text
-	k , r = extract_effective(msg)
-	sql_command = """INSERT INTO message (keyword, response)
-	VALUES ("{key}" , "{resp}");"""
-	sql_command = sql_command.format(key = k , resp = r)
-	cursor.execute(sql_command)
-	connection.commit()
-	connection.close()
-	bot.send_message(chat_id = chat_id , text = 'command successfully added!')
+
+
+def filter_resp(bot, update):
+    connection = sqlite3.connect("sql/filter.db")
+    cursor = connection.cursor()
+    chat_id = update.message.chat_id
+    msg = update.effective_message.text
+    k, r = extract_effective(msg)
+    sql_command = """INSERT INTO message (keyword, response)
+    VALUES ("{key}" , "{resp}");"""
+    sql_command = sql_command.format(key=k, resp=r)
+    cursor.execute(sql_command)
+    connection.commit()
+    bot.send_message(chat_id=chat_id, text='command successfully added!')
 
 # Responds to the message when a filtered keyword is encountered
-def filter_reply(bot , update):
-	connection = sqlite3.connect("sql/filter.db")
-	cursor = connection.cursor()
-	msg = update.effective_message.text
-	for word in msg.split(' ')[1:]:
-		cursor.execute("SELECT * FROM message WHERE keyword=?", (word,))
-		rows = cursor.fetchall()
-		if(len(rows) > 0):
-			message = update.effective_message
-			message.reply_text(rows[0][1])
-	connection.close()
+
+
+def filter_reply(bot, update):
+    connection = sqlite3.connect("sql/filter.db")
+    cursor = connection.cursor()
+    msg = update.effective_message.text
+    for word in msg.split(' ')[1:]:
+        cursor.execute("SELECT * FROM message WHERE keyword=?", (word,))
+        rows = cursor.fetchall()
+        if(len(rows) > 0):
+            message = update.effective_message
+            message.reply_text(rows[0][1])
+    connection.close()
+
 
 def main(TOKEN):
-        try:
-                updater = Updater(TOKEN)
-                dp = updater.dispatcher
-                BOT_COMMANDS = {'start':startmessage,
-                                 'sendlogo':sendlogo,
-                                 'addmeeting':add_meeting,
-                                 'next':get_list,
-                                 'filter':filter_resp,
-                                 'reply':filter_reply,
-                                 'thonkify':thonkify,
-                                 'help':helpbot,
-                                 'social':add_socialmedia,
-                                 'remove':remove_user,
-                                 'facebook':getlink,
-                                 'github':getlink,
-                                 'website':getlink,
-                                 'meetup':getlink}
-                for key,value in BOT_COMMANDS.items():
-                        dp.add_handler(CommandHandler(key ,value))
-                dp.add_handler(MessageHandler(Filters.status_update, welcome_message))
-                updater.start_polling()
-                updater.idle()
-        except:
-                print("The Token Is Invalid! Use Correct Token in config.ini file")
+    try:
+        updater = Updater(TOKEN)
+        dp = updater.dispatcher
+        BOT_COMMANDS = {'start': startmessage,
+                        'sendlogo': sendlogo,
+                        'addmeeting': add_meeting,
+                        'next': get_list,
+                        'filter': filter_resp,
+                        'reply': filter_reply,
+                        'thonkify': thonkify,
+                        'help': helpbot,
+                        'social': add_socialmedia,
+                        'remove': remove_user,
+                        'facebook': getlink,
+                        'github': getlink,
+                        'website': getlink,
+                        'meetup': getlink}
+        for key, value in BOT_COMMANDS.items():
+            dp.add_handler(CommandHandler(key, value))
+        dp.add_handler(MessageHandler(Filters.status_update, welcome_message))
+        updater.start_polling()
+        updater.idle()
+    except:
+        print("The Token Is Invalid! Use Correct Token in config.ini file")
+
 
 if __name__ == '__main__':
-        TOKEN = CheckConfig()
-        if TOKEN:        
-                main(TOKEN)
+    TOKEN = CheckConfig()
+    if TOKEN:
+        main(TOKEN)
